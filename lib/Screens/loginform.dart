1af971bb303e37b68c1164d41ea5ef7cdcee309f
bibek-in-github <mahatcoding@gmail.com>
2023-07-04 08:19:47 -0700

    added image

--- lib/Screens/loginform.dart
+++ lib/Screens/loginform.dart
@@ -12,18 +12,28 @@ class LoginForm extends StatelessWidget {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 30.0,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50.0),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 30.0,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10.0),
+                Image.asset(
+                  'assests/images/logo.jpg',
+                  height: 80.0,
+                  width: 80.0,
+                ),
+                 SizedBox(height: 10.0),
+              ],
+            ),
           ),
         ),
       ),
