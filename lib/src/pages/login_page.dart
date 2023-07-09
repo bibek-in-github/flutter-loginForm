6f649b1c06ce561221168e34804f3c2db5b87d50
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-07-09 22:28:01 +0545

    feat:login page

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart'
+    show
+        AppBar,
+        Axis,
+        BorderRadius,
+        BoxDecoration,
+        BuildContext,
+        Colors,
+        Column,
+        Container,
+        EdgeInsets,
+        Icons,
+        MainAxisAlignment,
+        Row,
+        Scaffold,
+        SingleChildScrollView,
+        SizedBox,
+        StatelessWidget,
+        Text,
+        TextButton,
+        TextEditingController,
+        TextInputType,
+        TextStyle,
+        Widget;
+import '../app.dart';
+import '../widgets/molecules/index.dart';
+import 'signup_page.dart';
+
+class LoginPage extends StatelessWidget {
+  static const routeName = "login_page";
+
+  final _conUserId = TextEditingController();
+  final _conPassword = TextEditingController();
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login with signup'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LoginSignupHelper(
+              headerName: 'Login',
+            ),
+            CustomTextFormField(
+              controller: _conUserId,
+              hintname: 'User ID',
+              icon: Icons.person,
+              inputType: TextInputType.number,
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            CustomTextFormField(
+              controller: _conPassword,
+              hintname: 'Password',
+              icon: Icons.lock,
+              isObscureText: true,
+              inputType: TextInputType.visiblePassword,
+            ),
+            Container(
+              margin: const EdgeInsets.all(30.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Does not have account?'),
+                TextButton(
+                  onPressed: () {
+                    mainNavigator.currentState?.pushNamed(SignupPage.routeName);
+                  },
+                  child: const Text(
+                    'Signup',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
